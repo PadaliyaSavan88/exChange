8ec35941295943382e167d0df21865ca417723c1
PadaliyaSavan88 <padaliyasavan8@gmail.com>
2022-05-30 00:28:20 +0530

    transfer function added and test file updated

--- test/Token.test.js
+++ test/Token.test.js
@@ -1,18 +1,22 @@
+import { tokens } from './helper'
+
 const Token = artifacts.require('./Token');
 
 require('chai')
     .use(require('chai-as-promised'))
     .should()
 
-contract('Token', (accounts) => {
-    const name = 'My Name';
-    const symbol = 'Symbol';
+
+
+contract('Token', ([deployer, receiver]) => {
+    const name = 'Spik Token';
+    const symbol = 'SPIK';
     const decimal = '18';
-    const totaSupply = '10000000000000000000000';
+    const totalSupply = tokens(1000000).toString();
     let token;
 
     beforeEach(async () => {
-        token = new Token();
+        token = await Token.new();
     })
 
     describe('deployment', () => {
@@ -28,13 +32,36 @@ contract('Token', (accounts) => {
         })
 
         it('tracks the decimal', async () => {
-            const result = await token.decimal();
+            const result = await token.decimals();
             result.toString().should.equal(decimal);
         })
 
         it('tracks the total supply', async () => {
             const result = await token.totalSupply();
-            result.toString().should.equal(totalSupply);
+            result.toString().should.equal(totalSupply.toString());
+        })
+
+        it('assign the totalSupply to the developer', async () => {
+            const result = await token.balanceOf(deployer);
+            result.toString().should.equal(totalSupply.toString());
+        })
+    })
+
+    describe('sending tokens', () => {
+        let result
+        let amount
+        beforeEach(async () => {
+            amount = tokens(100);
+            result = await token.transfer(receiver, amount, { from: deployer })
+        })
+        
+        it('transfer token balance', async () => {
+            let balanceOf;
+
+            balanceOf = await token.balanceOf(deployer);
+            balanceOf.toString().should.equal(tokens(999900).toString());
+            balanceOf = await token.balanceOf(receiver);
+            balanceOf.toString().should.equal(tokens(100).toString());        
         })
     })
 })
