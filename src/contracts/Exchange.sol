a91f9afa5331c4ad90957e213d8204db2221c623
SavanElluminati <savanelluminati@gmail.com>
2022-09-15 20:03:55 +0530

    create order and canel order added

--- src/contracts/Exchange.sol
+++ src/contracts/Exchange.sol
@@ -11,10 +11,43 @@ contract Exchange {
     uint256 public feePercent; // fee percentage
     address constant ETHER = address(0);
     mapping(address => mapping(address => uint256)) public tokens;
+    mapping(uint256 => _Order) public orders;
+    mapping(uint256 => bool) public orderCancelled;
+    uint256 public orderCount;
 
     // Events
     event Deposit(address token, address user, uint256 amount, uint256 balance);
     event Withdraw(address token, address user, uint256 amount, uint256 balance);
+    event Order(
+        uint id,
+        address user,
+        address tokenGet,
+        uint amountGet,
+        address tokenGive,
+        uint amountGive,
+        uint timestamp
+    );
+
+    event Cancle(
+        uint id,
+        address user,
+        address tokenGet,
+        uint amountGet,
+        address tokenGive,
+        uint amountGive,
+        uint timestamp
+    );
+
+    // Structs
+    struct _Order {
+        uint id;
+        address user;
+        address tokenGet;
+        uint amountGet;
+        address tokenGive;
+        uint amountGive;
+        uint timestamp;
+    }
 
     constructor (address _feeAccount, uint256 _feePercentage) public {
         feeAccount = _feeAccount;
@@ -55,6 +88,20 @@ contract Exchange {
 
     function balanceOf(address _token, address _user) public view returns (uint256) {
         return tokens[_token][_user];
+    }
+
+    function makeOrder(address _tokenGet, uint256 _amountGet, address _tokenGive, uint256 _amountGive) public {
+        orderCount = orderCount.add(1);
+        orders[orderCount] = _Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+        emit Order(orderCount, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now);
+    }
+
+    function cancelOrder(uint256 _id) public {
+        _Order storage _order = orders[_id];
+        require(address(_order.user) == msg.sender);
+        require(_order.id == _id);
+        orderCancelled[_id] == true;
+        emit Cancel(_order.id, msg.sender, _tokenGet, _amountGet, _tokenGive, _amountGive, now )
     }
 
 }
